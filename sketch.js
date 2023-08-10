9b17005c7a07883da389abad5e9ebd3f813b1be0
annikazitto <112134638+annikazitto@users.noreply.github.com>
2023-08-10 11:44:53 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let sound, amplitude;
+
+function preload() {
+  sound = loadSound("NorthernCardinalisolated.wav");
+}
+
+function setup() {
+  
+  let cnv = createCanvas(600,600);
+  cnv.mouseClicked(togglePlay);
+
+  amplitude = new p5.Amplitude();
+
+}
+
+function draw() {
+  
+  background('#C62710');
+  noStroke();
+
+  let level = amplitude.getLevel();
+  let size = map(level, 0, 1, 0, 600);
+  
+  fill('#000000')  
+  ellipse(width/2, height/2, size*150, size*150);
+   
+  fill('#FF5722')  
+  ellipse(width/2, height/2, size*40, size*40);
+ 
+
+
+}
+
+function togglePlay() {
+  if (sound.isPlaying() ){
+    sound.pause();
+  } else {
+    sound.loop();
+		amplitude = new p5.Amplitude();
+		amplitude.setInput(sound);
+  }
+}
