8133aca7f600aa202bb93b3033937f255b40e534
Renee Walker <54253764+renwalk@users.noreply.github.com>
2024-05-30 15:20:33 -0400

    Update sketch.js
    Update sketch to include default size for graphic

--- sketch.js
+++ sketch.js
@@ -5,38 +5,49 @@ function preload() {
 }
 
 function setup() {
-  
   let cnv = createCanvas(400,400);
   cnv.mouseClicked(togglePlay);
-
   amplitude = new p5.Amplitude();
-
 }
 
 function draw() {
   
-  background('#C62710');
+  
+  background('#C62710'); // Moved background to the beginning
   noStroke();
 
-  let level = amplitude.getLevel();
-  let size = map(level, 0, 1, 0, 400);
   
-  fill('#000000')  
-  ellipse(width/2, height/2, size*150, size*150);
-   
-  fill('#FF5722')  
-  ellipse(width/2, height/2, size*40, size*40);
- 
+  let size = 2; // Default size
+  
+  if (sound.isPlaying()) {
+    let level = amplitude.getLevel();
+    size = map(level, 0, 1, 0, 300);
+  }  
 
+  //Wing
+  fill('#8A4436');
+  ellipse(width / 2, height / 2, size * 100, size * 100);
 
+  //Belly
+  fill('#E74B31');
+  ellipse(width / 2, height / 2, size * 70, size * 70);
+  
+   //Head
+  fill('#2D0C0A');
+  ellipse(width / 2, height / 2, size * 30, size * 30);
+  
+  //Beak
+  fill('#F6321C');
+  ellipse(width / 2, height / 2, size * 10, size* 10);
+  
 }
 
 function togglePlay() {
-  if (sound.isPlaying() ){
+  if (sound.isPlaying()) {
     sound.pause();
   } else {
     sound.loop();
-		amplitude = new p5.Amplitude();
-		amplitude.setInput(sound);
+    amplitude = new p5.Amplitude();
+    amplitude.setInput(sound);
   }
 }
